941196b2c71506e17d9d1bfcf905ce3b874c81b0
Cyber Security Nighty <146417216+cybersecuritynighty@users.noreply.github.com>
2026-09-03 03:36:07 +0700

    Update Program.cs

--- ECommerceRex/Program.cs
+++ ECommerceRex/Program.cs
@@ -7,6 +7,14 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDistributedMemoryCache(); // or use Redis if you prefer
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 // Add services
 builder.Services.AddControllersWithViews();
 
