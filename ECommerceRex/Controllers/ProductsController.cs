f5673df3caa01ba37c835821932a6e5222233749
Cyber Security Nighty <146417216+cybersecuritynighty@users.noreply.github.com>
2026-09-03 02:30:15 +0700

    Update ProductsController.cs

--- ECommerceRex/Controllers/ProductsController.cs
+++ ECommerceRex/Controllers/ProductsController.cs
@@ -32,9 +32,31 @@ public class ProductsController : Controller
         return View(products);
     }
 
-    public IActionResult CRUD() => View();
-    public IActionResult Supply() => View();
-    public IActionResult Category() => View();
+    public IActionResult CRUD() { 
+        return View(); 
+    }
+    
+    public IActionResult Supply() {
+        return View();
+    }
+    
+    public IActionResult Category() {
+        return View();
+    }
 
     // Additional CRUD actions omitted for brevity (would include Create, Edit, Delete)
+    [HttpPost]
+    [Authorize(Roles = "Admin")] // Optional: restrict to admins
+    public async Task<IActionResult> Delete(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product != null)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            // Invalidate cache
+            await _cache.RemoveAsync("all_products");
+        }
+        return RedirectToAction(nameof(Index));
+    }
 }
