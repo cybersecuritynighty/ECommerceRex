9614accab3f08027d58eedb25d1245231706b889
Cyber Security Nighty <146417216+cybersecuritynighty@users.noreply.github.com>
2026-09-02 21:21:29 +0700

    Create IRedisCacheService.cs

---
+++ ECommerceRex/Services/IRedisCacheService.cs
@@ -0,0 +1,8 @@
+namespace ECommerceRex.Services;
+
+public interface IRedisCacheService
+{
+    Task<T?> GetAsync<T>(string key);
+    Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
+    Task RemoveAsync(string key);
+}
