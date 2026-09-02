796a1bbdd24cb9050fd9727a5d493e4e92275734
Cyber Security Nighty <146417216+cybersecuritynighty@users.noreply.github.com>
2026-09-03 03:15:03 +0700

    Update Program.cs

--- ECommerceRex/Program.cs
+++ ECommerceRex/Program.cs
@@ -97,6 +97,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapFallbackToController("NotFound", "Home");
+
 // Ensure database created and seeded (optional)
 using (var scope = app.Services.CreateScope())
 {
