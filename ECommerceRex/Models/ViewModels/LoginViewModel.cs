fc53cb00c0d5b05ef592624955e175f49cd53ca5
Cyber Security Nighty <146417216+cybersecuritynighty@users.noreply.github.com>
2026-09-02 21:12:20 +0700

    Create LoginViewModel.cs

---
+++ ECommerceRex/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceRex.Models.ViewModels;
+
+public class LoginViewModel
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
